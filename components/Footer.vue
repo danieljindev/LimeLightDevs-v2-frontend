3d462028a79f173766d1245a5cf40c1e7bf04c14
Ethanol <ethanolchennault@gmail.com>
2020-10-17 23:51:49 -0700

    created footer

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- the footer is placed below all the content in its parent if they
+  overflows, and if it does not overflow footer is placed at bottom. 
+  however, the parent of the footer has to have `flex flex-col min-h-screen`
+  It is NOT fixed. -->
+  <div
+    class="mt-auto px-0 sm:px-4 md:px-12 lg:px-24 py-10 w-screen text-white bg-gray-900 flex items-center justify-center sm:justify-between"
+  >
+    <!-- a text replica of the logo, probably want to replace it with an img, this way it can scale better as well -->
+    <div class="flex items-end justify-center text-5xl tracking-wide">
+      <span class="font-bold">Lime</span>
+      <span class="tracking-wide">light</span>
+      <span class="pl-4 text-xl font-semibold text-gray-300">devs</span>
+    </div>
+    <div
+      class="hidden mx-4 sm:flex flex-col items-center justify-center text-center"
+    >
+      <div class="pb-1 md:text-xl lg:text-4xl font-semibold tracking-wide">
+        LimeLight devs - Registered 2020
+      </div>
+      <div
+        class="text-center font-semibold md:text-md lg:text-xl text-gray-400"
+      >
+        App pagesLorem ipsum - semper vehicula.
+      </div>
+    </div>
+    <p
+      class="hidden sm:flex items-center justify-center font-semibold sm:text-md md:text-lg lg:text-xl text-gray-400"
+    >
+      support@limelightdevs.com
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
