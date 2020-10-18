a4c65b6cf424f14deda94205dc5fe8b92526396b
Ethanol <ethanolchennault@gmail.com>
2020-10-18 14:31:19 -0700

    mobile css

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,27 +1,27 @@
 <template>
   <div
-    class="mt-auto px-0 sm:px-4 md:px-12 lg:px-24 py-10 text-white bg-gray-900 flex items-center justify-center sm:justify-between"
+    class="mt-auto px-0 sm:px-4 md:px-12 lg:px-24 py-6 sm:py-10 text-white bg-app-gray-2 flex flex-col sm:flex-row items-center justify-center sm:justify-between"
   >
     <!-- a text replica of the logo, probably want to replace it with an img, this way it can scale better as well -->
-    <div class="flex items-end justify-center text-5xl tracking-wide">
+    <div class="hidden sm:flex items-end justify-center text-5xl tracking-wide">
       <span class="font-bold">Lime</span>
       <span class="tracking-wide">light</span>
       <span class="pl-4 text-xl font-semibold text-gray-300">devs</span>
     </div>
     <div
-      class="hidden mx-4 sm:flex flex-col items-center justify-center text-center"
+      class="pb-4 sm:pb-0 flex flex-col items-center justify-center text-center"
     >
-      <div class="pb-1 md:text-xl lg:text-3xl font-semibold tracking-wide">
+      <div class="pb-1 text-xl lg:text-3xl font-semibold tracking-wide">
         LimeLight devs - Registered 2020
       </div>
       <div
-        class="text-center font-semibold md:text-md lg:text-xl text-gray-400"
+        class="text-center font-semibold text-sm md:text-md lg:text-xl text-gray-400"
       >
         App pagesLorem ipsum - semper vehicula.
       </div>
     </div>
     <p
-      class="hidden sm:flex items-center justify-center font-semibold sm:text-md md:text-lg lg:text-xl text-gray-400"
+      class="flex items-center justify-center font-semibold text-sm sm:text-md md:text-lg lg:text-xl text-gray-400"
     >
       support@limelightdevs.com
     </p>
