cc175cfac6eb8d9a110c67b2d607b068786d3e76
justintd <justintdinh1@gmail.com>
2020-10-19 08:32:50 -0700

    Updated font size

--- components/NavBar/NavMobile.vue
+++ components/NavBar/NavMobile.vue
@@ -15,13 +15,13 @@
           <a
             v-if="!link.button"
             :href="link.href"
-            class="text-lg font-bold hover:text-app-green-1 transition duration-200"
+            class="text-xl font-bold hover:text-app-green-1 transition duration-200"
           >
             {{ link.name }}
           </a>
           <button
             v-else
-            class="px-5 py-1 text-lg text-app-gray-1 font-bold rounded-full bg-app-green-1 hover:text-app-green-1 hover:bg-app-gray-1 border-2 border-app-green-1 transition-all ease-in-out duration-200 focus:outline-none"
+            class="px-5 py-1 text-xl text-app-gray-1 font-bold rounded-full bg-app-green-1 hover:text-app-green-1 hover:bg-app-gray-1 border-2 border-app-green-1 transition-all ease-in-out duration-200 focus:outline-none"
           >
             <a :href="link.href">{{ link.name }}</a>
           </button>
