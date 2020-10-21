10f9c722633e58b12f31d55b59b0447497950685
TNORTHERN <traynorthern@yahoo.com>
2020-10-21 07:23:41 -0500

    Even more reusable process items and misc page content

--- components/landing/process/ProcessPoint.vue
+++ components/landing/process/ProcessPoint.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="w-1/6">
+  <div class="w-1/6 relative z-20">
     <div class="flex justify-center">
       <div
         class="rounded-full h-20 w-20 bg-app-gray-2 flex items-center justify-center"
       >
-        <img class="w-16" src="/processes/launch.png" alt="" />
+        <img class="w-16" :src="src" alt="" />
       </div>
     </div>
     <div class="text-center">
       <h4 class="font-bold">Wireframes & designs</h4>
       <p>
-        App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames
-        semper vehicula.
+        {{ body }}
       </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    body: {
+      type: String,
+      default: '',
+    },
+  },
+}
 </script>
 
 <style></style>
--- pages/process.vue
+++ pages/process.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    <div class="flex">
-      <ProcessPoint />
-      <ProcessPoint />
-      <ProcessPoint />
+  <div class="container mx-auto px-3">
+    <h2 class="text-4xl font-bold mb-5">Our Process</h2>
+    <div class="flex justify-between">
+      <div class="absolute">
+        <DottedLine />
+      </div>
+      <ProcessPoint
+        src="/processes/wireframes.png"
+        body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
+      />
+      <ProcessPoint
+        src="/processes/development.png"
+        body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
+      />
+      <ProcessPoint
+        src="/processes/launch.png"
+        body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
+      />
     </div>
   </div>
 </template>
