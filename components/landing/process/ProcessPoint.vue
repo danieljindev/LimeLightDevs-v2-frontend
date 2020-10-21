121811780a314dad9df06d5ff5a218ee46a5b365
TNORTHERN <traynorthern@yahoo.com>
2020-10-21 07:13:01 -0500

    Reusable process items

---
+++ components/landing/process/ProcessPoint.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-1/6">
+    <div class="flex justify-center">
+      <div
+        class="rounded-full h-20 w-20 bg-app-gray-2 flex items-center justify-center"
+      >
+        <img class="w-16" src="/processes/launch.png" alt="" />
+      </div>
+    </div>
+    <div class="text-center">
+      <h4 class="font-bold">Wireframes & designs</h4>
+      <p>
+        App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames
+        semper vehicula.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/process.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="flex">
+      <ProcessPoint />
+      <ProcessPoint />
+      <ProcessPoint />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
