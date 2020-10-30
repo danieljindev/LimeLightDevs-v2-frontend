0effa2153e4f209c83ed0daed5ea0cce7aa47df6
Ethanol <ethanolchennault@gmail.com>
2020-10-30 00:20:58 -0700

    finished styling

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-full text-white flex justify-start">
-    <img src="hero/blob.png" class="absolute top-0 right-0 w-2/3 h-auto" />
-    <img src="hero/projects.png" class="absolute right-0 w-2/5 h-auto" />
-    <!-- honestly thinking of just creating a whole new image with all the images put on it, it seems to difficult to responsively style this -->
+  <div class="w-full text-white flex mb-32 lg:mb-64">
+    <img src="hero/blob.png" class="absolute top-0 right-0 w-full lg:w-2/3 h-auto" />
+    <img src="hero/projects.png" class="hidden lg:block absolute right-0 w-full lg:w-2/5 h-auto" />
+    <!-- probably be best to just include one image rather than style a bunch on top of each other -->
     <div
-      class="relative w-1/2 flex flex-col items-center lg:items-start lg:ml-56 my-64 text-center lg:text-left"
+      class="relative lg:w-1/2 flex flex-col items-start lg:ml-56 my-32 lg:my-48 pl-8 lg:pl-0"
     >
-      <span class="text-8xl font-hibana w-full mb-1">Welcome!</span>
-      <span class="text-6xl font-hibana text-app-green-1 w-full mb-6">Limelight - Web Developing</span>
-      <p class="text-lg w-3/4 mb-8">
+      <span class="text-6xl lg:text-8xl font-hibana mb-4 lg:mb-2">Welcome!</span>
+      <span class="text-4xl lg:text-6xl font-hibana text-app-green-1 mb-4 lg:mb-8">Limelight - Web Developing</span>
+      <p class="text-lg leading-tight lg:leading-normal lg:text-2xl w-5/6 mb-8">
         Lorem ipsum dolor sit amet consectetur adipiscing elit, fames semper
         vehicula velit dapibus in iaculis. Eros cursus lobortis leo hendrerit
         faucibus parturient sagittis justo curabitur lacinia ridiculus class.
@@ -20,8 +20,9 @@
         Contact Us
       </a>
 
-      <div class="h-full flex items-center justify-center absolute left-0">
-      <span class="text-app-gray-3 font-bold text-xl ml-6 transform -rotate-90 -translate-x-40 -translate-y-10">contact@limelight.com</span>
+      <!-- a very weird way to style it correctly, but it works -->
+      <div class="flex h-full items-center justify-center mt-8 lg:mt-0 lg:absolute lg:left-0 pointer-events-none">
+        <span class="text-app-gray-3 font-bold text-2xl transform lg:-rotate-90 lg:-translate-x-40 lg:-translate-y-10">contact@limelight.com</span>
       </div>
     </div>
   </div>
