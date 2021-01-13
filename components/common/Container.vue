701f769e12c80893e66c4c26cdf8138cd5aebd29
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-01-13 18:54:31 -0500

    Create Container.vue

---
+++ components/common/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- DESIGN VARIANTS  -->
+  <div
+    class="mx-auto mt-10 md:mt-24"
+    :class="[
+      variant === 'one' ? 'w-10/12' : '',
+      variant === 'two' ? 'w-10/12 lg:w-4/5 xl:w-3/5 2xl:w-7/12' : '',
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: 'one',
+    },
+  },
+}
+</script>
