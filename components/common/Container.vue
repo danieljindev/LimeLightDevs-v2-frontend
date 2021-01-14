2a9b5890865a0fa8807c2bce430692afdbf97ba8
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-01-13 21:36:55 -0500

    Update Container.vue

--- components/common/Container.vue
+++ components/common/Container.vue
@@ -3,7 +3,7 @@
   <div
     class="mx-auto mt-10 md:mt-24"
     :class="[
-      variant === 'one' ? 'w-10/12' : '',
+      variant === 'one' ? 'w-10/12 lg:w-9/12 xl:w-4/6 2xl:w-3/5' : '',
       variant === 'two' ? 'w-10/12 lg:w-4/5 xl:w-3/5 2xl:w-7/12' : '',
     ]"
   >
