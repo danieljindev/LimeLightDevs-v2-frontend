1d05a9c533929aae296f88c7488b503516570892
tnorthern <traynorthern@yahoo.com>
2021-01-24 12:14:27 -0600

    change

--- pages/process.vue
+++ pages/process.vue
@@ -2,17 +2,16 @@
   <div class="container mx-auto px-3">
     <h2 class="text-4xl font-bold mb-5">Our Process</h2>
     <div class="flex justify-between">
-      <div class="absolute">
-        <DottedLine />
-      </div>
       <ProcessPoint
         src="/processes/wireframes.png"
         body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
       />
+      <!-- <left-dotted-line class="w-2/5 -ml-40" /> -->
       <ProcessPoint
         src="/processes/development.png"
         body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
       />
+      <!-- <right-dotted-line class="w-2/5 -ml-20" /> -->
       <ProcessPoint
         src="/processes/launch.png"
         body="App pagesLorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula."
@@ -22,7 +21,11 @@
 </template>
 
 <script>
-export default {}
+// import LeftDottedLine from '~/components/misc/icons/LeftDottedLine.vue'
+// import RightDottedLine from '~/components/misc/icons/RightDottedLine.vue'
+export default {
+  // components: { RightDottedLine, LeftDottedLine },
+}
 </script>
 
 <style></style>
