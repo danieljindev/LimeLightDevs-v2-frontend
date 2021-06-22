3634061d5c4ba03de65e425b7392c2e1dcaf0032
Daniel Jin <danieljin123@outlook.com>
2021-06-22 03:40:29 +0300

    Add fetch hook to work component

--- components/works/WorkCarousel.vue
+++ components/works/WorkCarousel.vue
@@ -1,12 +1,17 @@
 <template>
-  <div v-if="loading"><loading-spinner /></div>
-  <div v-else>
-    <VueSlickCarousel v-bind="settings">
-      <div v-for="(project, index) in projects" :key="index" class="relative px-3 py-2 focus:outline-none">
-        <img :src="'/works/3.jpg'" class="w-full object-cover rounded-xl" draggable="false" />
-      </div>
-    </VueSlickCarousel>
-  </div>
+  <VueSlickCarousel v-bind="settings">
+    <div
+      v-for="(project, index) in projects"
+      :key="index"
+      class="relative px-3 py-2 focus:outline-none"
+    >
+      <img
+        :src="'/works/3.jpg'"
+        class="w-full object-cover rounded-xl"
+        draggable="false"
+      />
+    </div>
+  </VueSlickCarousel>
 </template>
 
 <script>
@@ -14,26 +19,11 @@ import { mapState } from 'vuex'
 import VueSlickCarousel from 'vue-slick-carousel'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-import LoadingSpinner from '../common/LoadingSpinner.vue'
 
 export default {
-  name: 'MyComponent',
-  components: { LoadingSpinner, VueSlickCarousel },
-  computed: {
-    ...mapState({
-      loading: (state) => state.works.loading,
-      projects: (state) => state.works.projects,
-    }),
-  },
-  created() {
-    this.$store.dispatch('works/getProjects')
-  },
-  methods: {
-    selectProject(projectId) {
-      this.$store.commit('works/selectProject', projectId)
-    },
-  },
-  setup() {
+  name: 'WorkCarousel',
+  components: { VueSlickCarousel },
+  data() {
     const settings = {
       dots: false,
       arrows: false,
@@ -51,6 +41,11 @@ export default {
     return {
       settings,
     }
+  },
+  computed: {
+    ...mapState({
+      projects: (state) => state.works.projects,
+    }),
   },
 }
 </script>
