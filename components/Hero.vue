331b1b486dfbb4b07a4450413b06d31c5b017907
Daniel Jin <danieljin123@outlook.com>
2021-06-28 20:48:18 +0300

    Fix home page text align issue (#161)

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,7 +10,20 @@
     />
     <!-- probably be best to just include one image rather than style a bunch on top of each other -->
     <div
-      class="relative w-full lg:w-1/2 flex flex-col items-center lg:items-start lg:ml-[3.8rem] xl:ml-56 my-24 lg:my-36 pl-8 lg:pl-0"
+      class="
+        relative
+        w-full
+        lg:w-1/2
+        flex flex-col
+        items-center
+        lg:items-start
+        lg:ml-[3.8rem]
+        xl:ml-56
+        my-24
+        lg:my-36
+        pl-0
+        lg:pl-8
+      "
     >
       <span class="text-6xl md:text-5xl font-hibana mb-4 lg:mb-2"
         >Welcome!</span
@@ -19,24 +32,57 @@
         class="text-2xl sm:text-3xl font-hibana text-app-green-1 mb-4 lg:mb-8"
         >Limelight - Web Developing</span
       >
-      <p class="text-md lg:text-lg w-5/6 mb-8">
+      <p class="text-md lg:text-lg w-5/6 mb-8 text-center lg:text-left">
         Lorem ipsum dolor sit amet consectetur adipiscing elit, fames semper
         vehicula velit dapibus in iaculis. Eros cursus lobortis leo hendrerit
         faucibus parturient sagittis justo curabitur lacinia ridiculus class.
       </p>
       <a
         href="#contact"
-        class="px-5 md:px-8 py-2 tracking-wider font-bold text-xl text-app-green-1 border-2 transition duration-200 border-app-green-1 rounded-full cursor-pointer focus:outline-none hover:text-white hover:bg-app-green-1"
+        class="
+          px-5
+          md:px-8
+          py-2
+          tracking-wider
+          font-bold
+          text-xl text-app-green-1
+          border-2
+          transition
+          duration-200
+          border-app-green-1
+          rounded-full
+          cursor-pointer
+          focus:outline-none
+          hover:text-white
+          hover:bg-app-green-1
+        "
       >
         Contact Us
       </a>
 
       <!-- a very weird way to style it correctly, but it works -->
       <div
-        class="flex h-full items-center justify-center mt-8 lg:mt-0 lg:absolute lg:left-0 pointer-events-none"
+        class="
+          flex
+          h-full
+          items-center
+          justify-center
+          mt-8
+          lg:mt-0
+          lg:absolute
+          lg:left-0
+          pointer-events-none
+        "
       >
         <span
-          class="text-white lg:text-app-gray-3 font-bold text-lg transform lg:-rotate-90 lg:-translate-x-32 lg:-translate-y-10"
+          class="
+            text-white
+            lg:text-app-gray-3
+            font-bold
+            text-lg
+            transform
+            lg:-rotate-90 lg:-translate-x-32 lg:-translate-y-10
+          "
           >contact@limelight.com</span
         >
       </div>
