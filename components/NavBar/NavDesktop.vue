d9d20059e0d64f097debdc2480e8d374ac07c2ad
Daniel Jin <danieljin123@outlook.com>
2021-07-02 00:52:04 +0300

    Update contact us link style

--- components/NavBar/NavDesktop.vue
+++ components/NavBar/NavDesktop.vue
@@ -6,10 +6,34 @@
         <nuxt-link
           v-if="!link.button"
           :to="link.href"
-          class="font-bold text-white hover:text-app-green-1 transition ease-in-out duration-300"
+          class="
+            font-bold
+            text-white
+            hover:text-app-green-1
+            transition
+            ease-in-out
+            duration-300
+          "
           >{{ link.name }}</nuxt-link
         >
-        <Button v-else variant="inverse">
+        <Button
+          v-else
+          class="
+            px-5
+            py-1
+            text-xl text-app-gray-1
+            font-bold
+            rounded-full
+            bg-app-green-1
+            hover:text-app-green-1
+            hover:bg-app-gray-1
+            border-2 border-app-green-1
+            transition-all
+            ease-in-out
+            duration-200
+            focus:outline-none
+          "
+        >
           <nuxt-link :to="link.href">{{ link.name }}</nuxt-link>
         </Button>
       </li>
