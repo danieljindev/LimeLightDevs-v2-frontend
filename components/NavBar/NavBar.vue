13c3fdb101c4b126bfc28b053c9dafd5bec67913
Daniel Jin <danieljin123@outlook.com>
2021-07-08 06:36:23 +0300

    Add fixed header (#175)
    * Add fixed header
    
    * Add header shadow, update header background
    
    * Make header transparent initially

--- components/NavBar/NavBar.vue
+++ components/NavBar/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container mx-auto py-8 px-6 xl:px-48 2xl:px-4">
+  <div
+    class="container navbar py-8 px-6 xl:px-48 2xl:px-4"
+    :class="{ 'navbar--unpinned': scrolled, 'shadow-xl': scrolled }"
+  >
     <!-- so navbar always takes priority, will actually be absolute in the future for a few reasons -->
     <div class="flex items-center justify-between relative z-40">
       <NavLogo />
@@ -30,7 +33,44 @@ export default {
         button: true,
       },
     },
+    limitPosition: 100,
+    scrolled: false,
+    lastPosition: 0,
   }),
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      if (this.limitPosition < window.scrollY) {
+        this.scrolled = true
+      } else {
+        this.scrolled = false
+      }
+
+      this.lastPosition = window.scrollY
+    },
+  },
 }
 </script>
-<style></style>
+<style>
+.navbar {
+  position: fixed;
+  z-index: 29;
+  padding-right: 4%;
+  padding-left: 4%;
+  max-width: 100%;
+  transition: all 0.4s ease;
+}
+.navbar--pinned {
+  transform: translateY(0%);
+}
+.navbar--unpinned {
+  background: rgb(34, 34, 34);
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
