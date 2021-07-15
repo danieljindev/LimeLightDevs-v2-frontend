1666b4cf085afde2cd601d058d91066f347202fc
tnorthern <traynorthern@yahoo.com>
2021-07-15 11:35:42 -0500

    Real data

--- components/works/WorksComponentDesktop.vue
+++ components/works/WorksComponentDesktop.vue
@@ -1,7 +1,20 @@
 <template>
-  <section-container class="relative z-[1] hidden sm:block" :default-margins="false">
+  <section-container
+    class="relative z-[1] hidden sm:block"
+    :default-margins="false"
+  >
     <section-title text="Our Works" />
-    <div class="sm:flex flex-wrap items-stretch rounded-2xl overflow-hidden relative hidden">
+    <div
+      class="
+        sm:flex
+        flex-wrap
+        items-stretch
+        rounded-2xl
+        overflow-hidden
+        relative
+        hidden
+      "
+    >
       <div v-for="work in projects" :key="work.id" class="w-1/4">
         <works-card :work="work" />
       </div>
